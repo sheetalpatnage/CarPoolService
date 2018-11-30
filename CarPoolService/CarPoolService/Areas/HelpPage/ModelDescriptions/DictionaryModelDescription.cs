59e60fdb2b7a24ab4aefbfe741a369de08e6f663
psheetal <Sheetal.Patange@esosolutions.com>
2018-11-30 11:50:48 +0530

    Initial Commit

---
+++ CarPoolService/CarPoolService/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CarPoolService.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CarPoolService/CarPoolService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarPoolService.WebApiApplication" Language="C#" %>
